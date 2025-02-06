ca1271cb5a875a303f059272f7e8425498661377
Giorgio Zoppi <sba23101@student.cct.ie>
2025-02-06 11:16:30 +0100

    new file:   README.md 	modified:   ../for_loop/src/main.rs 	streamsync/ 	../matrix_mul/

--- for_loop/src/main.rs
+++ for_loop/src/main.rs
@@ -3,4 +3,22 @@ fn main() {
     for item in message {
         println!("Char : {}", item);
     }
+    println!("Let's print also the index");
+    for (index, item) in message.iter().enumerate() {
+        println!("{} {}", index, item);
+    }
+    println!("I wanna print until i don't find l");
+    for (index, &item) in message.iter().enumerate() {
+        if item == 'l' {
+            break;
+        }
+        println!("{} {}", index, item);
+    }
+    println!("i want to print the even number till 20");
+    for n in 1..20 {
+        if n % 2 == 0 {
+            println!("{}", n);
+        }
+    }
+
 }
