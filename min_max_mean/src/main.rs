c22ac2799475661a4bc217bc905cb889aad5dad8
Giorgio Zoppi <sba23101@student.cct.ie>
2025-02-06 11:24:47 +0100

    Add mininum, average, maximum.

---
+++ min_max_mean/src/main.rs
@@ -0,0 +1,16 @@
+fn main() {
+    let values = [2,80,5,6,7,8,10, 2];
+    let mut sum = 0;
+    let mut max = i32::MIN;
+    let mut min = i32::MAX;
+    for v in values {
+        sum+=v;
+        if v < min {
+            min = v;
+        }
+        if v > max  {
+            max = v;
+        }
+    }
+    println!("Minumum {}, Max {}, Average {}", min, max, sum / (values.len() as i32) );
+}
